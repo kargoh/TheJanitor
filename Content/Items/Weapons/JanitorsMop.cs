e53f804aaf41f17a10342f4fa770ca5c694a30e1
kargoh <deben6@gmail.com>
2022-08-11 11:08:26 -0700

    first commit

---
+++ Content/Items/Weapons/JanitorsMop.cs
@@ -0,0 +1,51 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace TheJanitor.Content.Items.Weapons
+{
+    internal class JanitorsMop : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Janitor's Mop");
+            Tooltip.SetDefault("He mops but never cleans");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            // Hitbox
+            Item.width = 64;
+            Item.height = 64;
+
+            // Use and Animation style
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.autoReuse = false;
+
+            // Damage Values
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 20;
+            Item.knockBack = 1.5f;
+
+            // Misc
+            Item.value = Item.buyPrice(copper: 1);
+            Item.rare = ItemRarityID.Blue;
+
+            // Sound
+            Item.UseSound = SoundID.Item1;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.DirtBlock, 1) // Adds ingredients and quantity
+                .AddTile(TileID.Anvils) // Adds crafting station
+                .Register(); // Registers the item
+        }
+    }
+}
