c9431101bdab1c64ea92c8753d12aa74fe62eac4
Jacob DeBenedetto <deben3@gmail.com>
2022-08-11 11:31:56 -0700

    Add age to damage option

--- Content/Items/Weapons/JanitorsMop.cs
+++ Content/Items/Weapons/JanitorsMop.cs
@@ -29,7 +29,7 @@ namespace TheJanitor.Content.Items.Weapons
 
             // Damage Values
             Item.DamageType = DamageClass.Melee;
-            Item.damage = 20;
+            Item.damage = JanitorsMop.GetDamage();
             Item.knockBack = 1.5f;
 
             // Misc
@@ -46,6 +46,18 @@ namespace TheJanitor.Content.Items.Weapons
                 .AddIngredient(ItemID.DirtBlock, 1) // Adds ingredients and quantity
                 .AddTile(TileID.Anvils) // Adds crafting station
                 .Register(); // Registers the item
+        }
+
+        public static int GetDamage()
+        {
+            // Use the janitors age as the damage (ex: 1991 = 30 y/o)
+            var today = DateTime.Today;
+            var birthday = DateTime.Parse("8.16.1991");
+            var age = today.Year - birthday.Year;
+
+            // Go back to the year in which the person was born in case of a leap year
+            if (birthday.Date > today.AddYears(-age)) age--;
+            return age;
         }
     }
 }
